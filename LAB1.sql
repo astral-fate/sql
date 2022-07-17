21d13a6f192d026248061901c23629f98b388392
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2022-07-17 08:10:18 +0300

    Update LAB1.sql

--- LAB1.sql
+++ LAB1.sql
@@ -1,1 +1,58 @@
-SELECT * FROM aq$_internet_agent_privs
+CREATE TABLE `employee` (
+  `fname` string,
+  `minit` number,
+  `lname` string,
+  `ssn` number,
+  `bdate` date,
+  `address` string,
+  `sex` string,
+  `super_ssn` integer,
+  `dno` number
+);
+
+CREATE TABLE `department` (
+  `dname` string,
+  `dnumber` number,
+  `mgr_ssn` number,
+  `mgr_start_date` date
+);
+
+CREATE TABLE `dept_locations` (
+  `dnumber` number,
+  `dlocation` string
+);
+
+CREATE TABLE `projects` (
+  `pname` integer,
+  `pnumber` string,
+  `plocation` string,
+  `dnum` number
+);
+
+CREATE TABLE `works_on` (
+  `essn` number,
+  `pno` number,
+  `hours` number
+);
+
+CREATE TABLE `dependent` (
+  `essn` number,
+  `dependent_name` string,
+  `sex` string,
+  `bdate` date,
+  `relationship` string
+);
+
+ALTER TABLE `department` ADD FOREIGN KEY (`dnumber`) REFERENCES `employee` (`dno`);
+
+ALTER TABLE `employee` ADD FOREIGN KEY (`ssn`) REFERENCES `department` (`mgr_ssn`);
+
+ALTER TABLE `dept_locations` ADD FOREIGN KEY (`dnumber`) REFERENCES `department` (`dnumber`);
+
+ALTER TABLE `projects` ADD FOREIGN KEY (`dnum`) REFERENCES `department` (`dnumber`);
+
+ALTER TABLE `works_on` ADD FOREIGN KEY (`essn`) REFERENCES `employee` (`ssn`);
+
+ALTER TABLE `works_on` ADD FOREIGN KEY (`pno`) REFERENCES `projects` (`pnumber`);
+
+ALTER TABLE `dependent` ADD FOREIGN KEY (`essn`) REFERENCES `employee` (`ssn`);
