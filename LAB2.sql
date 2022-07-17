a42fb518e635d73dd601de229d9ed6051f3c0dc1
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2022-07-17 14:08:32 +0300

    Update LAB2.sql

--- LAB2.sql
+++ LAB2.sql
@@ -1,47 +1,50 @@
-CREATE TABLE `employee` (
-  `fname` string,
-  `minit` number,
-  `lname` string,
-  `ssn` number,
-  `bdate` date,
-  `address` string,
-  `sex` string,
-  `super_ssn` integer,
-  `dno` number
+CREATE TABLE employee (
+  fname CHAR(10),
+  minit NUMBER(10),
+  lname CHAR(10),
+  ssn NUMBER(10),
+  bdate date,
+  address CHAR(10),
+  sex CHAR(10),
+  super_ssn NUMBER(10),
+  dno INT PRIMARY KEY
 );
 
-CREATE TABLE `department` (
-  `dname` string,
-  `dnumber` number,
-  `mgr_ssn` number,
-  `mgr_start_date` date
+
+
+CREATE TABLE department (
+  dname CHAR(10),
+  dNUMBER INT PRIMARY KEY,
+  mgr_ssn NUMBER(10),
+  mgr_start_date date
 );
 
-CREATE TABLE `dept_locations` (
-  `dnumber` number,
-  `dlocation` string
+CREATE TABLE dept_locations (
+  dNUMBER INT PRIMARY KEY,
+  dlocation CHAR(10)
 );
 
-CREATE TABLE `projects` (
-  `pname` integer,
-  `pnumber` string,
-  `plocation` string,
-  `dnum` number
+CREATE TABLE projects (
+  pname NUMBER(10),
+  pNUMBER INT PRIMARY KEY,
+  plocation CHAR(10),
+  dnum INT PRIMARY KEY
 );
 
-CREATE TABLE `works_on` (
-  `essn` number,
-  `pno` number,
-  `hours` number
+CREATE TABLE works_on (
+  essn NUMBER(10),
+  pno INT PRIMARY KEY,
+  hours NUMBER(10)
 );
 
-CREATE TABLE `dependent` (
-  `essn` number,
-  `dependent_name` string,
-  `sex` string,
-  `bdate` date,
-  `relationship` string
+CREATE TABLE dependent (
+  essn NUMBER(10),
+  dependent_name CHAR(10),
+  sex CHAR(10),
+  bdate DATE,
+  relationship CHAR(10)
 );
+
 
 ALTER TABLE `department` ADD FOREIGN KEY (`dnumber`) REFERENCES `employee` (`dno`);
 
